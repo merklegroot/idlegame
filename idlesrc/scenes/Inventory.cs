12ee6957f29182adc3ebb8d20ea0784877c83c70
Merkle Groot <merklegroot@gmail.com>
2025-05-17 09:55:01 -0400

    dynamic inventory

--- idlesrc/scenes/Inventory.cs
+++ idlesrc/scenes/Inventory.cs
@@ -1,16 +1,32 @@
 using Godot;
+using System.Collections.Generic;
 
 namespace IdleGame;
 
 public partial class Inventory : Control
 {
-	private Label _woodCount;
-	private Label _stoneCount;
+	private VBoxContainer _resourceContainer;
+	private Dictionary<string, Label> _resourceLabels = new();
 	
 	public override void _Ready()
 	{
-		_woodCount = GetNode<Label>("VBoxContainer/Resources/WoodCount");
-		_stoneCount = GetNode<Label>("VBoxContainer/Resources/StoneCount");
+		_resourceContainer = GetNode<VBoxContainer>("VBoxContainer/Resources");
+
+		// Create labels for each resource
+		foreach (var resource in ResourceData.Instance.ListResources())
+		{
+			var label = new Label
+			{
+				HorizontalAlignment = HorizontalAlignment.Center,
+				ThemeTypeVariation = "",
+			};
+			
+			// Set font size using theme override
+			label.AddThemeConstantOverride("font_size", 32);
+			
+			_resourceContainer.AddChild(label);
+			_resourceLabels[resource.Id] = label;
+		}
 		
 		// Connect to inventory changes
 		GameState.Instance.InventoryChanged += UpdateDisplay;
@@ -20,7 +36,23 @@ public partial class Inventory : Control
 	
 	private void UpdateDisplay()
 	{
-		_woodCount.Text = $"Wood: {GameState.Instance.GetResouceQuantity("wood")}";
-		_stoneCount.Text = $"Stone: {GameState.Instance.GetResouceQuantity("stone")}";
+		foreach (var (resourceId, label) in _resourceLabels)
+		{
+			var resourceInfo = ResourceData.Instance.GetResourceById(resourceId);
+			var quantity = GameState.Instance.GetResouceQuantity(resourceId);
+			label.Text = $"{resourceInfo.Name}: {quantity}";
+		}
+	}
+
+	public override void _ExitTree()
+	{
+		foreach (var label in _resourceLabels.Values)
+		{
+			if (label != null && IsInstanceValid(label))
+			{
+				label.QueueFree();
+			}
+		}
+		_resourceLabels.Clear();
 	}
 } 

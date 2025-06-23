02ab286bf7da7bdfe50a718ed4f83e7c425ae3c2
Merkle Groot <merklegroot@gmail.com>
2025-06-23 09:26:18 -0400

    inventory list panel

---
+++ idlesrc/scenes/Inventory/InventoryListPanel.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace IdleGame;
+
+public partial class InventoryListPanel : Control
+{
+    public override void _Ready()
+    {
+        // Initialize the list panel
+        // This will be expanded as we add more functionality
+    }
+} 

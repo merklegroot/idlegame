58b328ada936095cf910cdda16979fc51b60622d
Merkle Groot <merklegroot@gmail.com>
2025-06-23 09:21:40 -0400

    inventory detail panel

---
+++ idlesrc/scenes/Inventory/InventoryDetailPanel.cs
@@ -0,0 +1,12 @@
+using Godot;
+
+namespace IdleGame;
+
+public partial class InventoryDetailPanel : Control
+{
+    public override void _Ready()
+    {
+        // Initialize the detail panel
+        // This will be expanded as we add more functionality
+    }
+} 

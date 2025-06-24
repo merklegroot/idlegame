9ae50338221cd07ab1b3d8734339c5e1289231bd
Merkle Groot <merklegroot@gmail.com>
2025-06-24 11:02:54 -0400

    more alignment

--- idlesrc/scenes/Inventory/Inventory.cs
+++ idlesrc/scenes/Inventory/Inventory.cs
@@ -1,57 +1,10 @@
 using Godot;
-using System.Collections.Generic;
-using IdleGame.Models;
-using IdleGame.Models.Messages;
 
 namespace IdleGame;
 
 public partial class Inventory : Control
 {
-	private VBoxContainer _resourceContainer;
-	private List<InventoryListItem> _resourceLines = new();
-	
 	public override void _Ready()
 	{
-		_resourceContainer = GetNode<VBoxContainer>("VBoxContainer/HSplitContainer/InventoryListSectionContainer/InventoryListItemContainer");
-		var inventoryLinePath = "res://scenes/Inventory/InventoryListItem.tscn";
-		var inventoryLineScene = GD.Load<PackedScene>(inventoryLinePath);
-
-		// Clean up any existing lines
-		foreach (var line in _resourceLines)
-		{
-			if (line != null && IsInstanceValid(line))
-			{
-				line.QueueFree();
-			}
-		}
-		_resourceLines.Clear();
-
-		// Create lines for each resource
-		foreach (var resource in ResourceData.Instance.ListResources())
-		{
-			var line = inventoryLineScene.Instantiate<InventoryListItem>();
-			line.ResourceId = resource.Id;
-			_resourceContainer.AddChild(line);
-			_resourceLines.Add(line);
-		}
-
-		GameEvent.InventoryChanged += OnInventoryChanged;
-	}
-
-	public override void _ExitTree()
-	{
-		foreach (var line in _resourceLines)
-		{
-			if (line != null && IsInstanceValid(line))
-			{
-				line.QueueFree();
-			}
-		}
-		_resourceLines.Clear();
-	}
-
-	private void OnInventoryChanged(InventoryChangedMessage request)
-	{
-		GD.Print($"Inventory changed: {request.ResourceId} {request.Quantity}");
 	}
 } 
--- idlesrc/scenes/Inventory/InventoryListPanel.cs
+++ idlesrc/scenes/Inventory/InventoryListPanel.cs
@@ -1,12 +1,57 @@
 using Godot;
+using System.Collections.Generic;
+using IdleGame.Models;
+using IdleGame.Models.Messages;
 
 namespace IdleGame;
 
 public partial class InventoryListPanel : Control
 {
+    private VBoxContainer _resourceContainer;
+    private List<InventoryListItem> _resourceLines = new();
+    
     public override void _Ready()
     {
-        // Initialize the list panel
-        // This will be expanded as we add more functionality
+        _resourceContainer = GetNode<VBoxContainer>("Panel/VBoxContainer");
+        var inventoryLinePath = "res://scenes/Inventory/InventoryListItem.tscn";
+        var inventoryLineScene = GD.Load<PackedScene>(inventoryLinePath);
+
+        // Clean up any existing lines
+        foreach (var line in _resourceLines)
+        {
+            if (line != null && IsInstanceValid(line))
+            {
+                line.QueueFree();
+            }
+        }
+        _resourceLines.Clear();
+
+        // Create lines for each resource
+        foreach (var resource in ResourceData.Instance.ListResources())
+        {
+            var line = inventoryLineScene.Instantiate<InventoryListItem>();
+            line.ResourceId = resource.Id;
+            _resourceContainer.AddChild(line);
+            _resourceLines.Add(line);
+        }
+
+        GameEvent.InventoryChanged += OnInventoryChanged;
+    }
+
+    public override void _ExitTree()
+    {
+        foreach (var line in _resourceLines)
+        {
+            if (line != null && IsInstanceValid(line))
+            {
+                line.QueueFree();
+            }
+        }
+        _resourceLines.Clear();
+    }
+
+    private void OnInventoryChanged(InventoryChangedMessage request)
+    {
+        GD.Print($"InventoryListPanel: Inventory changed: {request.ResourceId} {request.Quantity}");
     }
 } 
